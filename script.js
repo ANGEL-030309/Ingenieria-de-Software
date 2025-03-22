c50f10a9c1bf47b721d1e63049455e7588c4a9b3
Angel Guevara <130310134+AngelGuevara04@users.noreply.github.com>
2025-03-21 22:17:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 document.querySelector('.login-btn').addEventListener('click', function() {
     alert('Redirigiendo a la página de inicio de sesión...');
 });
+
+// Registro de usuarios
+const registrarUsuario = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log("Usuario registrado:", userCredential.user);
+    })
+    .catch((error) => {
+      console.error("Error al registrar:", error.message);
+    });
+};
+
+// Inicio de sesión
+const iniciarSesion = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log("Usuario conectado:", userCredential.user);
+      window.location.href = 'pagina-posterior.html'; // Redirige a otra página
+    })
+    .catch((error) => {
+      console.error("Error al iniciar sesión:", error.message);
+    });
+};
